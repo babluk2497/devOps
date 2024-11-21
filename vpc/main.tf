5be414af795f33330124d6c03c5843063e10d371
ravi2krishna <ravi2krishna@gmail.com>
2024-11-21 11:02:22 +0530

    create NACL

--- vpc/main.tf
+++ vpc/main.tf
@@ -92,3 +92,30 @@ resource "aws_route_table_association" "login-db-asc" {
   subnet_id      = aws_subnet.login-db-sn.id
   route_table_id = aws_route_table.login-pvt-rt.id
 }
+
+# NACL
+resource "aws_network_acl" "login-nacl" {
+  vpc_id = aws_vpc.login-vpc.id
+
+  egress {
+    protocol   = "tcp"
+    rule_no    = 100
+    action     = "allow"
+    cidr_block = "0.0.0.0/0"
+    from_port  = 0
+    to_port    = 65535
+  }
+
+  ingress {
+    protocol   = "tcp"
+    rule_no    = 100
+    action     = "allow"
+    cidr_block = "0.0.0.0/0"
+    from_port  = 0
+    to_port    = 65535
+  }
+
+  tags = {
+    Name = "login-nacl"
+  }
+}
