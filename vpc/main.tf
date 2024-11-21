184413044bcec9c2d8933f736e280ab7d8f849d2
ravi2krishna <ravi2krishna@gmail.com>
2024-11-21 10:35:04 +0530

    checking id for vpc

--- vpc/main.tf
+++ vpc/main.tf
@@ -6,3 +6,13 @@ resource "aws_vpc" "login-vpc" {
     Name = "login-vpc"
   }
 }
+
+# Frontend Subnet
+resource "aws_subnet" "login-fe-sn" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.0.0/24"
+
+  tags = {
+    Name = "login-frontend-subnet"
+  }
+}
