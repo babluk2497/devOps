162780451056889cd627725767cd3ede5a1b77ef
Naveen081198 <knaveeninfo98@gmail.com>
2024-11-21 17:22:34 +0530

    aws terraform

---
+++ vpc/variables.tf
@@ -0,0 +1,10 @@
+#variables
+variables "aws_access_key" {
+    type = string
+    description = "Enter user access key"
+}
+
+variables "aws_secret_key" {
+    type = string
+    description = "Enter user secret key"
+}
