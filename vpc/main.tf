8f69d5252c6b34deef9c2502f5eadf2dee3b4a9d
ravi2krishna <ravi2krishna@gmail.com>
2024-11-21 10:40:38 +0530

    enable public ip for subnet

--- vpc/main.tf
+++ vpc/main.tf
@@ -11,6 +11,8 @@ resource "aws_vpc" "login-vpc" {
 resource "aws_subnet" "login-fe-sn" {
   vpc_id     = aws_vpc.login-vpc.id
   cidr_block = "10.0.0.0/24"
+  availability_zone = "us-west-2a"
+  map_public_ip_on_launch = "true"
 
   tags = {
     Name = "login-frontend-subnet"
