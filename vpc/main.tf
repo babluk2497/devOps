6d2a7fe863bb5c2919e6d83ca5d7c6a569de1b95
Naveen081198 <knaveeninfo98@gmail.com>
2024-11-22 17:28:19 +0530

    vpc subnet

--- vpc/main.tf
+++ vpc/main.tf
@@ -1,8 +1,17 @@
 #VPC
-resource "aws_vpc" "main-vpc" {
+resource "aws_vpc" "login-vpc" {
   cidr_block       = "10.0.0.0/16"
 
   tags = {
-    Name = "main-vpc"
+    Name = "login-vpc"
+  }
+}
+
+resource "aws_subnet" "login-fe-subnet" {
+  vpc_id     = aws_vpc_ipv4_cidr_block_association.login-vpc.vpc-vpc_id
+  cidr_block = "10.0.1.0/24"
+
+  tags = {
+    Name = "login-fe-subnet"
   }
 }
