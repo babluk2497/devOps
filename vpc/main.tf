99d8f9c2162e002f55bdb8f46699efd955ebb650
ravi2krishna <ravi2krishna@gmail.com>
2024-11-25 11:15:17 +0530

    update

--- vpc/main.tf
+++ vpc/main.tf
@@ -79,7 +79,7 @@ resource "aws_route_table_association" "login-db-asc" {
 
 # NACL
 resource "aws_network_acl" "login-nacl" {
-  vpc_id = aws_vpc.login-vpc.id
+  vpc_id = aws_vpc.vpc.id
 
   egress {
     protocol   = "tcp"
