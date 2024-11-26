214bb2841e342a1522cc5d087c1303c31261a1ed
ravi2krishna <ravi2krishna@gmail.com>
2024-11-26 10:46:07 +0530

    update names

--- ec2/main.tf
+++ ec2/main.tf
@@ -87,7 +87,7 @@ resource "aws_security_group" "login-fe-sg" {
 }
 
 # Frontend Rules
-resource "aws_vpc_security_group_ingress_rule" "login_web_ingress" {
+resource "aws_vpc_security_group_ingress_rule" "login_web_ingress_ssh" {
   security_group_id = aws_security_group.login-fe-sg.id
   cidr_ipv4         = "0.0.0.0/0"
   from_port         = 22
@@ -95,7 +95,7 @@ resource "aws_vpc_security_group_ingress_rule" "login_web_ingress" {
   to_port           = 22
 }
 
-resource "aws_vpc_security_group_ingress_rule" "login_web_ingress" {
+resource "aws_vpc_security_group_ingress_rule" "login_web_ingress_http" {
   security_group_id = aws_security_group.login-fe-sg.id
   cidr_ipv4         = "0.0.0.0/0"
   from_port         = 80
