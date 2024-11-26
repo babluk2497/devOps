890b4b1834c17a00ecaa32b218a7f15ab9898fd5
ravi2krishna <ravi2krishna@gmail.com>
2024-11-26 10:57:15 +0530

    update list

--- ec2/ec2.tf
+++ ec2/ec2.tf
@@ -4,7 +4,7 @@ resource "aws_instance" "login-web" {
   instance_type = "t2.micro"
   key_name      = "2429"
   subnet_id     = aws_subnet.public_subnets.id
-  vpc_security_group_ids = aws_security_group.login-fe-sg.id
+  vpc_security_group_ids = [aws_security_group.login-fe-sg.id]
 
   tags = {
     Name = "${var.vpc_name}-web-server"
